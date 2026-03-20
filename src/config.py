57897d18a081094a053769b4ef1ff7fa1ba9aedd
Yoon Hwang <yhwang55@wisc.edu>
2026-03-20 10:32:31 -0500

    Update src/config.py with new configurations.

--- src/config.py
+++ src/config.py
@@ -4,6 +4,7 @@ PROJECT_ROOT = Path(__file__).resolve().parents[1]
 DATA_DIR = PROJECT_ROOT / "data"
 SAMPLE_DATA_DIR = DATA_DIR / "sample"
 RAW_DATA_DIR = DATA_DIR / "raw"
+KAGGLE_DATA_DIR = RAW_DATA_DIR / "m5-forecasting-accuracy"
 PROCESSED_DATA_DIR = DATA_DIR / "processed"
 OUTPUT_DIR = PROJECT_ROOT / "outputs"
 MODEL_DIR = OUTPUT_DIR / "models"
